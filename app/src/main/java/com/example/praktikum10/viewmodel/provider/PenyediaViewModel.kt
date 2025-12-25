5ffde43fa4d1ce26ae1b104d52eec8fff27202d0
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-12-25 20:39:28 +0700

    menambahlkan  DetailViewModel initializer to PenyediaViewModel factory

--- app/src/main/java/com/example/praktikum10/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/praktikum10/viewmodel/provider/PenyediaViewModel.kt
@@ -1,9 +1,12 @@
 package com.example.praktikum10.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.mydatasiswa.viewmodel.DetailViewModel
+import com.example.mydatasiswa.viewmodel.EditViewModel
 import com.example.praktikum10.repositori.AplikasiDataSiswa
 import com.example.praktikum10.viewmodel.EntryViewModel
 import com.example.praktikum10.viewmodel.HomeViewModel
@@ -17,5 +20,19 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
         initializer { EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
+
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
+
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
     }
 }
